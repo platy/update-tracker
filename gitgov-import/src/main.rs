65d4532ce239e1425382b4822a0fe3aa80d8373b
Mike Bush <platy@njk.onl>
2021-02-12 20:55:14 +0000

    Start importing docs

--- gitgov-import/src/main.rs
+++ gitgov-import/src/main.rs
@@ -1,20 +1,29 @@
-use std::{fs::remove_dir_all, io, str::FromStr};
+use std::{
+    fs::remove_dir_all,
+    io,
+    str::{from_utf8, FromStr},
+};
 
 use anyhow::{bail, format_err, Context, Result};
 use chrono::{DateTime, NaiveDateTime, Utc};
-use git2::{Commit, Repository};
-use update_tracker::update::UpdateRepo;
+use git2::{Blob, Commit, Diff, Repository};
+use html5ever::serialize::{HtmlSerializer, Serialize, SerializeOpts, Serializer, TraversalScope};
+use io::{Read, Write};
+use update_tracker::{doc::DocRepo, update::UpdateRepo};
 use url::Url;
 
 fn main() -> Result<()> {
-    const REPO_BASE: &str = "./out/update";
-    remove_dir_all(REPO_BASE)?;
+    const UPDATE_REPO_BASE: &str = "./out/update";
+    const DOC_REPO_BASE: &str = "./out/doc";
+    let _ = remove_dir_all(UPDATE_REPO_BASE);
+    let _ = remove_dir_all(DOC_REPO_BASE);
 
     let repo = Repository::open(dotenv::var("GITGOV_REPO")?)?;
     let reference = repo.find_reference(&dotenv::var("GITGOV_REF")?)?;
     let mut commit = reference.peel_to_commit()?;
 
-    let mut update_repo = UpdateRepo::new(REPO_BASE)?;
+    let mut update_repo = UpdateRepo::new(UPDATE_REPO_BASE)?;
+    let mut doc_repo = DocRepo::new(DOC_REPO_BASE)?;
 
     loop {
         if commit.author().email().unwrap() == "info@gov.uk" {
@@ -22,7 +31,9 @@ fn main() -> Result<()> {
                 commit: &commit,
                 repo: &repo,
             };
-            if let Err(error) = import_commit(extractor, &mut update_repo) {
+            import_docs_from_commit(&extractor, &mut doc_repo)
+                .context(format!("Importing docs from {}", commit.id()))?;
+            if let Err(error) = import_updates_from_commit(&extractor, &mut update_repo) {
                 println!("Error on {} : {}", commit.id(), error);
                 if !error.to_string().contains("Too many files") {
                     break;
@@ -42,19 +53,19 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn import_commit(extractor: Extractor, repo: &mut UpdateRepo) -> Result<()> {
+fn import_updates_from_commit(extractor: &Extractor, update_repo: &mut UpdateRepo) -> Result<()> {
     // println!("import {}", extractor.commit.message().unwrap());
     let url = extractor.url()?;
     let ts = extractor.timestamp()?;
     let change = extractor.message()?;
     let _tag = extractor.tag()?;
-    match repo.create(url.clone(), ts, change) {
+    match update_repo.create(url.clone(), ts, change) {
         Ok((update, _events)) => {
             println!("create {:?}", &update);
             Ok(())
         }
         Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
-            let existing = repo.get_update(url, ts)?;
+            let existing = update_repo.get_update(url, ts)?;
             if existing.change() == change {
                 println!("exists {:?}", &existing);
                 Ok(())
@@ -70,16 +81,120 @@ fn import_commit(extractor: Extractor, repo: &mut UpdateRepo) -> Result<()> {
     }
 }
 
+fn import_docs_from_commit(extractor: &Extractor, doc_repo: &mut DocRepo) -> Result<()> {
+    // println!("import {}", extractor.commit.message().unwrap());
+    let docs = extractor.doc_versions().context("loading doc versions")?;
+    let ts = extractor.timestamp()?;
+    let _tag = extractor.tag()?;
+    for (url, blob) in docs {
+        let content = normalise(blob.content())?;
+        match doc_repo.create(url.clone(), ts) {
+            Ok(mut writer) => {
+                writer.write_all(content.as_bytes())?;
+                let (update, _events) = writer.done()?;
+                println!("create {:?}", &update);
+                Ok(())
+            }
+            Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
+                let existing = doc_repo.ensure_version(url, ts)?;
+                let mut data: Vec<u8> = vec![];
+                doc_repo.open(&existing)?.read_to_end(&mut data)?;
+                if data == content.as_bytes() {
+                    println!("exists {:?}", &existing);
+                    Ok(())
+                } else {
+                    // let diff = html_diff::get_differences(from_utf8(&data)?, from_utf8(blob.content())?); // TODO pre strip test data
+                    let diff = prettydiff::diff_lines(from_utf8(&data)?, from_utf8(blob.content())?);
+                    Err(format_err!("Update exists with different content : {}", diff,))
+                }
+            }
+            Err(err) => Err(err).context("error writing update"),
+        }?;
+    }
+    Ok(())
+}
+
+fn normalise(input: &[u8]) -> Result<String> {
+    let html = scraper::Html::parse_fragment(std::str::from_utf8(input)?);
+    let root = html.root_element();
+    let traversal_scope = if root.value().name.local == html5ever::local_name!("html") {
+        TraversalScope::ChildrenOnly(None)
+    } else {
+        TraversalScope::IncludeNode
+    };
+    let opts = SerializeOpts {
+        scripting_enabled: false, // It's not clear what this does.
+        traversal_scope,
+        create_missing_parent: false,
+    };
+    let mut buf = Vec::new();
+    let mut ser = NormalizingHtmlSerializer(HtmlSerializer::new(&mut buf, opts.clone()));
+    root.serialize(&mut ser, opts.traversal_scope)?;
+    Ok(String::from_utf8(buf).unwrap())
+}
+
+struct NormalizingHtmlSerializer<Wr: Write>(HtmlSerializer<Wr>);
+
+impl<Wr: Write> Serializer for NormalizingHtmlSerializer<Wr> {
+    fn start_elem<'a, AttrIter>(&mut self, name: html5ever::QualName, attrs: AttrIter) -> io::Result<()>
+    where
+        AttrIter: Iterator<Item = html5ever::serialize::AttrRef<'a>>,
+    {
+        let mut attrs: Vec<_> = attrs.collect();
+        attrs.sort_by(|(k1, _), (k2, _)| k1.cmp(k2));
+        self.0.start_elem(name, attrs.into_iter())
+    }
+
+    fn end_elem(&mut self, name: html5ever::QualName) -> io::Result<()> {
+        self.0.end_elem(name)
+    }
+
+    fn write_text(&mut self, text: &str) -> io::Result<()> {
+        self.0.write_text(text)
+    }
+
+    fn write_comment(&mut self, text: &str) -> io::Result<()> {
+        self.0.write_comment(text)
+    }
+
+    fn write_doctype(&mut self, name: &str) -> io::Result<()> {
+        self.0.write_doctype(name)
+    }
+
+    fn write_processing_instruction(&mut self, target: &str, data: &str) -> io::Result<()> {
+        self.0.write_processing_instruction(target, data)
+    }
+}
+
 struct Extractor<'r> {
     repo: &'r git2::Repository,
     commit: &'r git2::Commit<'r>,
 }
 
 impl<'r> Extractor<'r> {
-    fn url(&self) -> Result<Url> {
+    fn diff(&self) -> Result<Diff> {
         let tree = self.commit.tree()?;
         let parent_tree = self.commit.parents().next().as_ref().map(Commit::tree).transpose()?;
-        let diff = self.repo.diff_tree_to_tree(parent_tree.as_ref(), Some(&tree), None)?;
+        Ok(self.repo.diff_tree_to_tree(parent_tree.as_ref(), Some(&tree), None)?)
+    }
+
+    fn doc_versions(&self) -> Result<Vec<(Url, Blob)>> {
+        let mut v = vec![];
+        for diff in self.diff()?.deltas() {
+            let file = diff.new_file();
+            let path = file.path().unwrap();
+            let url = Url::from_str(&format!("https://www.gov.uk/{}", path.to_str().unwrap()))?;
+            let blob =
+                self.repo
+                    .find_blob(file.id())
+                    .context(format!("finding blob {} at path {:?}", file.id(), path))?;
+            v.push((url, blob));
+        }
+        Ok(v)
+    }
+
+    fn url(&self) -> Result<Url> {
+        let diff = self.diff()?;
         let files: Vec<_> = diff.deltas().collect();
         if files.len() != 1 {
             bail!("Too many files in commit {}", self.commit.id());

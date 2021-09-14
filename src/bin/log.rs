f6740d0525d0f33766ed3a16a8479f3a61dd78b1
Mike Bush <platy@njk.onl>
2021-09-14 19:49:45 +0000

    [log tool] Filter by absolute time range

--- src/bin/log.rs
+++ src/bin/log.rs
@@ -1,6 +1,11 @@
 use anyhow::*;
+use chrono::{Datelike, NaiveDate, NaiveDateTime};
 use clap::clap_app;
-use std::{collections::BTreeSet, convert::TryFrom};
+use std::{
+    collections::BTreeSet,
+    convert::TryFrom,
+    ops::{Bound, RangeBounds},
+};
 use url::Url;
 
 use update_tracker::{
@@ -31,7 +36,7 @@ fn main() -> Result<()> {
             .filter(|update_ref| {
                 update_ref
                     .as_ref()
-                    .map_or(true, |update_ref| filter.filter_update_ref(&update_ref))
+                    .map_or(true, |update_ref| filter.filter_update_ref(update_ref))
             })
             .collect::<Result<_, _>>()?;
         while let Some(tag) = filter.tags.pop() {
@@ -60,6 +65,8 @@ struct Filter {
     tags: Vec<Tag>,
     /// Filter to only updates on urls starting with this url prefix
     url_prefix: Option<Url>,
+    /// Filter to only updates published within a date range
+    date_range: (Bound<NaiveDateTime>, Bound<NaiveDateTime>),
 }
 
 impl<'s> TryFrom<Option<clap::Values<'s>>> for Filter {
@@ -68,6 +75,7 @@ impl<'s> TryFrom<Option<clap::Values<'s>>> for Filter {
     fn try_from(values: Option<clap::Values<'s>>) -> Result<Self, Self::Error> {
         let mut tags = vec![];
         let mut url_prefix = None;
+        let mut date_range = (Bound::Unbounded, Bound::Unbounded);
         if let Some(values) = values {
             for token in values {
                 if let Some(mut tag) = token.strip_prefix("#\"") {
@@ -81,19 +89,49 @@ impl<'s> TryFrom<Option<clap::Values<'s>>> for Filter {
                     tags.push(Tag::new(tag.to_owned()));
                 } else if token.starts_with("https://www.gov.uk/") {
                     url_prefix = Some(token.parse()?);
+                } else if let Some((from, to)) = token.split_once("..") {
+                    date_range = (
+                        Filter::parse_date_bound(from)?.map_or(Bound::Unbounded, Bound::Included),
+                        Filter::parse_date_bound(to)?.map_or(Bound::Unbounded, Bound::Excluded),
+                    );
+                } else {
+                    bail!("Unrecognised filter {}", token);
                 }
             }
         }
-        Ok(Filter { tags, url_prefix })
+        Ok(Filter {
+            tags,
+            url_prefix,
+            date_range,
+        })
     }
 }
+
 impl Filter {
-    pub(crate) fn filter_update_ref(&self, update_ref: &UpdateRef) -> bool {
+    fn filter_update_ref(&self, update_ref: &UpdateRef) -> bool {
         if let Some(url_prefix) = &self.url_prefix {
             if !update_ref.to_string().starts_with(&url_prefix.to_string()) {
                 return false;
             }
         }
-        true
+        self.date_range.contains(&update_ref.timestamp.naive_local())
+    }
+
+    fn parse_date_bound(s: &str) -> Result<Option<NaiveDateTime>> {
+        if s.is_empty() {
+            return Ok(None);
+        }
+        let mut date = NaiveDate::from_ymd(0, 1, 1);
+        let mut date_parts = s.split('-');
+        date = date
+            .with_year(date_parts.next().unwrap_or("").parse().context("Error parsing year")?)
+            .context("Invalid year")?;
+        if let Some(m) = date_parts.next().map(str::parse).transpose()? {
+            date = date.with_month(m).context("Error parsing month")?;
+        }
+        if let Some(d) = date_parts.next().map(str::parse).transpose()? {
+            date = date.with_day(d).context("Error parsing day")?;
+        }
+        Ok(Some(date.and_hms(0, 0, 0)))
     }
 }

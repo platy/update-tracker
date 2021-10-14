a703ff8a519cc1ae5146aed5a5007e0878e6d951
Mike Bush <platy@njk.onl>
2021-10-14 19:15:21 +0000

    [web] set etags for proper http caching

--- server/src/main.rs
+++ server/src/main.rs
@@ -37,13 +37,13 @@ fn main() {
             _ => {
                 if let Some(request) = request.remove_prefix("/update/") {
                     if let Some((timestamp, url)) = request.url().split_once('/') {
-                        return handle_update_page(timestamp, url, &data).unwrap_or_else(Into::into)
+                        return handle_update_page(&request, timestamp, url, &data).unwrap_or_else(Into::into)
                     }
                 } else if let Some(request) = request.remove_prefix("/diff/") {
                     let url = request.url();
                     let mut split = url.splitn(3, '/');
                     if let (Some(from), Some(to), Some(url)) = (split.next(), split.next(), split.next()) {
-                        return handle_doc_diff_page(from, to, url, &data).unwrap_or_else(Into::into)
+                        return handle_doc_diff_page(&request, from, to, url, &data).unwrap_or_else(Into::into)
                     }
                 }
                 Response::html("Not found").with_status_code(404)
@@ -55,10 +55,11 @@ fn main() {
 fn handle_updates(request: &Request, data: &Data) -> Response {
     let updates = data.list_updates();
 
-    Response::html(format!(include_str!("updates.html"), UpdateList(updates, request)))
+    let results = UpdateList(updates, request);
+    Response::html(format!(include_str!("updates.html"), results)).with_etag(request, results.etag())
 }
 
-fn handle_update_page(timestamp: &str, url: &str, data: &Data) -> Result<Response, Error> {
+fn handle_update_page(request: &Request, timestamp: &str, url: &str, data: &Data) -> Result<Response, Error> {
     let timestamp: DateTime<FixedOffset> = timestamp.parse().or(Err(Error::NotFound("Update")))?;
     let url: Url = format!("https://{}", url).parse().unwrap();
 
@@ -75,7 +76,7 @@ fn handle_update_page(timestamp: &str, url: &str, data: &Data) -> Result<Respons
             .max_by_key(|v| *v.timestamp())
     });
 
-    let (diff_url, from_ts, to_ts, body) = diff_fields(&url, previous_doc, current_doc, data);
+    let (diff_url, from_ts, to_ts, body) = diff_fields(&url, previous_doc.as_ref(), current_doc.as_ref(), data);
 
     Ok(Response::html(format!(
         include_str!("update.html"),
@@ -86,10 +87,11 @@ fn handle_update_page(timestamp: &str, url: &str, data: &Data) -> Result<Respons
         doc_from = from_ts.map_or(String::new(), |v| v.to_string()),
         doc_to = to_ts.map_or(String::new(), |v| v.to_string()),
         body = body,
-    )))
+    ))
+    .with_etag(request, format!("{} {}", previous_doc.is_some(), current_doc.is_some())))
 }
 
-fn handle_doc_diff_page(from: &str, to: &str, url: &str, data: &Data) -> Result<Response, Error> {
+fn handle_doc_diff_page(request: &Request, from: &str, to: &str, url: &str, data: &Data) -> Result<Response, Error> {
     let url: Url = format!("https://{}", url).parse().unwrap();
 
     // get doc version from
@@ -106,7 +108,7 @@ fn handle_doc_diff_page(from: &str, to: &str, url: &str, data: &Data) -> Result<
         .0;
     let to_doc = to.and_then(|ts| data.get_doc_version(&url, ts).ok());
 
-    let (diff_url, from_ts, to_ts, body) = diff_fields(&url, from_doc, to_doc, data);
+    let (diff_url, from_ts, to_ts, body) = diff_fields(&url, from_doc.as_ref(), to_doc.as_ref(), data);
 
     Ok(Response::html(format!(
         include_str!("diff.html"),
@@ -115,13 +117,14 @@ fn handle_doc_diff_page(from: &str, to: &str, url: &str, data: &Data) -> Result<
         doc_from = from_ts.map_or(String::new(), |v| v.to_string()),
         doc_to = to_ts.map_or(String::new(), |v| v.to_string()),
         body = body,
-    )))
+    ))
+    .with_etag(request, format!("{} {}", from_doc.is_some(), to_doc.is_some())))
 }
 
 fn diff_fields(
     url: &Url,
-    from: Option<DocumentVersion>,
-    to: Option<DocumentVersion>,
+    from: Option<&DocumentVersion>,
+    to: Option<&DocumentVersion>,
     data: &Data,
 ) -> (
     String,
@@ -131,22 +134,18 @@ fn diff_fields(
 ) {
     let diff_base = format!(
         "/diff/{}/{}/{}",
-        from.as_ref().map_or(String::new(), |v| v.timestamp().to_rfc3339()),
-        to.as_ref().map_or(String::new(), |v| v.timestamp().to_rfc3339()),
+        from.map_or(String::new(), |v| v.timestamp().to_rfc3339()),
+        to.map_or(String::new(), |v| v.timestamp().to_rfc3339()),
         url.host().unwrap(),
     );
 
-    let current_doc_body = to
-        .as_ref()
-        .map(|doc| data.read_doc_to_string(doc).with_base_url(&diff_base));
-    let previous_doc_body = from
-        .as_ref()
-        .map(|doc| data.read_doc_to_string(doc).with_base_url(&diff_base));
+    let current_doc_body = to.map(|doc| data.read_doc_to_string(doc).with_base_url(&diff_base));
+    let previous_doc_body = from.map(|doc| data.read_doc_to_string(doc).with_base_url(&diff_base));
 
     (
         format!("{}{}", diff_base, url.path()),
-        from.as_ref().map(DocumentVersion::timestamp).copied(),
-        to.as_ref().map(DocumentVersion::timestamp).copied(),
+        from.map(DocumentVersion::timestamp).copied(),
+        to.map(DocumentVersion::timestamp).copied(),
         match (previous_doc_body, current_doc_body) {
             (Some(previous_doc_body), Some(current_doc_body)) => previous_doc_body.diff(&current_doc_body),
             (Some(previous_doc_body), None) => previous_doc_body.into_inner(),
@@ -176,16 +175,8 @@ struct UpdateList<'a, U>(&'a [U], &'a Request);
 
 impl<'a, U: Borrow<Update>> fmt::Display for UpdateList<'a, U> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let offset = self
-            .1
-            .get_param("offset")
-            .and_then(|offset| offset.parse().ok())
-            .unwrap_or(0);
-        let limit = self
-            .1
-            .get_param("limit")
-            .and_then(|limit| limit.parse().ok())
-            .unwrap_or(200);
+        let offset = self.offset();
+        let limit = self.limit();
 
         let items = self.0;
         let start = items.len() - offset;
@@ -265,5 +256,26 @@ impl<'a, U: Borrow<Update>> fmt::Display for UpdateList<'a, U> {
         }
         writeln!(f, "</div>")?;
         Ok(())
+    }
+}
+
+impl<'a, U: Borrow<Update>> UpdateList<'a, U> {
+    fn limit(&self) -> usize {
+        self.1
+            .get_param("limit")
+            .and_then(|limit| limit.parse().ok())
+            .unwrap_or(200)
+    }
+
+    fn offset(&self) -> usize {
+        self.1
+            .get_param("offset")
+            .and_then(|offset| offset.parse().ok())
+            .unwrap_or(0)
+    }
+
+    fn etag(&self) -> String {
+        let offset = self.offset();
+        format!("{}", self.0[self.0.len() - offset - 1].borrow().timestamp())
     }
 }
